ac0d13efbcdde4455945d18151ad03f3ee796df6
fearoffun <480dunce@gmail.com>
2023-07-27 23:34:52 -0700

    editing files lol just worked on figuring out how to do the stylizing and also handlebar things

--- utils/auth.js
+++ utils/auth.js
@@ -1,6 +1,6 @@
 const withAuth = (req, res, next) => {
-    if (!req.session.signed_in) {
-      res.redirect('/signin');
+    if (!req.session.logged_in) {
+      res.redirect('/login');
     } else {
       next();
     }
